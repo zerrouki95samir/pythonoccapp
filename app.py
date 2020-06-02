5948e1f7b92068d192f271ab0f5601724a6cb32d
Zerrouki Samir <samir.etud.zerrouki@gmail.com>
2020-06-02 16:28:35 +0100

    every goes well except conda modules!

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
-from flask import Flask
-# , request, abort, jsonify, send_from_directory, make_response
-#from stp2json import stp2json
-'''
+from flask import Flask, request, abort, jsonify, send_from_directory, make_response
+from stp2json import stp2json
+
 UPLOAD_DIRECTORY = "./tmp"
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
-'''
+
 
 app = Flask(__name__)
 
@@ -17,7 +16,7 @@ def index():
     return '<h1>Hello, in python-occ-libs</h1>'
 
 
-'''
+
 @app.route("/files")
 def list_files():
     """Endpoint to list files on the server."""
@@ -51,6 +50,6 @@ def post_file(filename):
 
     return response
 
-'''
+
 if __name__ == "__main__":
     app.run(debug=True)
