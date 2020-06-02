d3bc4badc9df68d4e32dfab126f08d0810d82857
Zerrouki Samir <samir.etud.zerrouki@gmail.com>
2020-06-02 21:51:18 +0100

    push f_ck_u

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, abort, jsonify, send_from_directory, make_response
-#from stp2json import stp2json
+from stp2json import stp2json
 
 UPLOAD_DIRECTORY = "./tmp"
 
@@ -14,7 +14,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello, in python-occ-libs</h1>'
-
 
 
 @app.route("/files")
